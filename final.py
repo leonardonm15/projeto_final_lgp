bf7979f83fb27b047048ae6e86eb9b52e3d28918
Leonardo Nunes Muniz <leonardonm15@gmail.com>
2022-11-14 22:32:48 -0300

    parte de python do trabalho

---
+++ final.py
@@ -0,0 +1,60 @@
+import pandas as pd
+
+class Sistema:
+    def __init__(self) -> None:
+        self.dfs_dict = {}
+        self.merged_df = None
+    
+    def load_csv(self, path, nome_df):
+        try:
+            df = pd.read_csv(path, delimiter=';')
+            self.dfs_dict[nome_df] = df
+        except Exception as e:
+            print(e)
+
+    def show_df_names(self):
+        print("============= NOME DOS DF =============")
+        for key in self.dfs_dict.keys():
+            print(key)
+        print("=======================================")
+
+    def merge_tables(self, df1, df2, new_df):
+        print("============= COMEÇANDO MERGE =============")
+        try:
+            df = pd.merge(self.dfs_dict[df1], self.dfs_dict[df2], how="inner")
+            self.dfs_dict[new_df] = df
+            self.merged_df = new_df
+        except Exception as e:
+            print(e)
+        else:
+            print("============= MERGE  COMPLETO =============")
+
+    def check_var(self, **kwargs) -> str: # retorna o nome da variavel nao nula
+        # verifica qual a variavel nao tem valor nulo em dados_aluno()
+        # a vantagem disso é saber em qual df a gente faz a busca
+        # uma vez que ele pode nao ter dado merge ainda
+        for arg in kwargs:
+            if arg != None:
+                print("args, args_name", arg, " ", arg.__name__)
+                return (arg, arg.__name__)
+        return ValueError
+
+    def print_df(self, nome_df):
+        print("===================== PRINTANDO O DF ========================")
+        try:
+            self.dfs_dict[nome_df]
+        except:
+            print(f'=========== DF {nome_df} NAO EXISTE ===========')
+        else:
+            print(self.dfs_dict[nome_df])
+        print("=================================================================")
+
+    def dados_aluno(self, table, id_aluno=None, Nome=None):
+        print("========== COMEÇANDO BUSCA ===========")
+        try:
+            var_filtro_val= self.check_var(id_aluno, Nome)
+        except:
+            print("=== ERRO NO PARSING ===")        
+        print(self.dfs_dict[table].query(f"{var_filtro_val[0]} == {var_filtro_val[1]}"))
+        print("========== BUSCA FINALIZADA ==========")
+
---
+++ main.py
@@ -0,0 +1,21 @@
+from final import Sistema
+
+path1 = r".\nomes.csv"
+path2 = r".\notas.csv"
+
+sistema = Sistema()
+
+sistema.load_csv(path1, 'nomes')
+sistema.load_csv(path2, 'notas')
+
+sistema.show_df_names()
+sistema.print_df('nomes')
+sistema.print_df('notas')
+
+sistema.merge_tables("nomes", "notas", "notas_nomes")
+
+sistema.show_df_names()
+
+sistema.print_df("notas_nomes")
+
+sistema.dados_aluno("notas_nomes", id_aluno=1033221237)
